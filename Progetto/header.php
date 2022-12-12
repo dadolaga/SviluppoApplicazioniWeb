008641d43ec5568a030ad712b83747a09374d9e5
Dadolaga <lagazio.davide.1f@gmail.com>
2022-12-12 07:07:33 +0100

    finish index and modify header

--- Progetto/header.php
+++ Progetto/header.php
@@ -16,9 +16,13 @@
                 <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
             </form>
 
+            <div class="col-md-3 text-end">
+                <button type="button" class="btn btn-outline-primary me-2">Login</button>
+                <button type="button" class="btn btn-primary">Sign-up</button>
+            </div>
             <div class="dropdown text-end">
                 <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                    <img src="image/user.png" alt="mdo" width="32" height="32" class="rounded-circle">
                 </a>
                 <ul class="dropdown-menu text-small">
                     <li><a class="dropdown-item" href="#">My order</a></li>
--- Progetto/index.php
+++ Progetto/index.php
@@ -1,170 +1,158 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Bootstrap Example</title>
   <?php
-    require "connection.php";
-    require "include.php";
+  require "connection.php";
+  require "include.php";
   ?>
 
   <style>
     .text-truncate-container {
-    width: 250px;
+      width: 250px;
     }
+
     .text-truncate-container p {
-        -webkit-line-clamp: 3;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
+      -webkit-line-clamp: 3;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+    }
+
+    .testimonial-group > .row {
+      display: block;
+      overflow-x: auto;
+      white-space: nowrap;
+    }
+    .testimonial-group > .row > .col-auto {
+      display: inline-block;
     }
   </style>
 </head>
+
 <body>
-<div class="container">
-  <div id="carouselProducts" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
-          <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_184e1f24f10%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_184e1f24f10%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-          <div class="card-body d-flex flex-column align-items-start">
-            <h3 class="mb-3">
-              <a class="text-dark" href="#">Title 1</a>
-            </h3>
-            <div class="mb-2">36.25 €</div>
-            <p class="card-text mb-auto w-100 h-100" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Il testo (dal latino textus, "tessuto", "tram</div>
+  <?php require "header.php";?>
+  <div class="container">
+    <div id="carouselProducts" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselProducts" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselProducts" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselProducts" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_184e1f24f10%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_184e1f24f10%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+            <div class="card-body d-flex flex-column align-items-start">
+              <h3 class="mb-3">
+                <a class="text-dark" href="#">Title 3</a>
+              </h3>
+              <div class="mb-2">36.25 €</div>
+              <p class="card-text mb-auto w-100 h-100" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Il testo (dal latino textus, "tessuto", "tram
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_184e1f24f10%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_184e1f24f10%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+            <div class="card-body d-flex flex-column align-items-start">
+              <h3 class="mb-3">
+                <a class="text-dark" href="#">Title 3</a>
+              </h3>
+              <div class="mb-2">36.25 €</div>
+              <p class="card-text mb-auto w-100 h-100" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Il testo (dal latino textus, "tessuto", "tram
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_184e1f24f10%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_184e1f24f10%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+            <div class="card-body d-flex flex-column align-items-start">
+              <h3 class="mb-3">
+                <a class="text-dark" href="#">Title 3</a>
+              </h3>
+              <div class="mb-2">36.25 €</div>
+              <p class="card-text mb-auto w-100 h-100" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Il testo (dal latino textus, "tessuto", "tram
+            </div>
+          </div>
         </div>
       </div>
-      <div class="carousel-item active">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
-          <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_184e1f24f10%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_184e1f24f10%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-          <div class="card-body d-flex flex-column align-items-start">
-            <h3 class="mb-3">
-              <a class="text-dark" href="#">Title 2</a>
-            </h3>
-            <div class="mb-2">36.25 €</div>
-            <p class="card-text mb-auto w-100 h-100" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Il testo (dal latino textus, "tessuto", "tram</div>
-        </div>
-      </div>
-      <div class="carousel-item"><div class="carousel-item active">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
-          <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_184e1f24f10%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_184e1f24f10%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-          <div class="card-body d-flex flex-column align-items-start">
-            <h3 class="mb-3">
-              <a class="text-dark" href="#">Title 3</a>
-            </h3>
-            <div class="mb-2">36.25 €</div>
-            <p class="card-text mb-auto w-100 h-100" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Il testo (dal latino textus, "tessuto", "tram</div>
-        </div>
-      </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselProducts" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselProducts" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </a>
-  </div>
-  <!--<div>
-    <div class="card flex-md-row mb-4 box-shadow h-md-250">
-      <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_184e1f24f10%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_184e1f24f10%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-      <div class="card-body d-flex flex-column align-items-start">
-        <h3 class="mb-3">
-          <a class="text-dark" href="#">Title</a>
-        </h3>
-        <div class="mb-2">36.25 €</div>
-        <p class="card-text mb-auto w-100 h-100" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Il testo (dal latino textus, "tessuto", "tram</div>
-    </div>
-  </div> -->
-  <div class="alert alert-danger">
-    <strong>Danger!</strong> This alert box could indicate a dangerous or potentially negative action.
-    <input type="submit" class="btn btn-info" value="Submit Button"><!--bottone login-->
-    <a href="#">Comments <span class="badge">10</span></a><br>
-
-    <ul class="pagination pagination-sm">
-  <li><a href="#">1</a></li>
-  <li class="active"><a href="#">2</a></li>
-  <li><a href="#">3</a></li>
-  <li><a href="#">4</a></li>
-  <li><a href="#">5</a></li>
-
-
-
-  <div class="dropdown">
-  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-  <span class="caret"></span></button>
-  <ul class="dropdown-menu">
-    <li><a href="#">HTML</a></li>
-    <li><a href="#">CSS</a></li>
-    <li><a href="#">JavaScript</a></li>
-  </ul>
-</div>
-</ul>
-
-
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
-  </div>
-</nav>
-
-<!--search-->
-<form>
-  <div class="input-group">
-    <input type="text" class="form-control" placeholder="Search">
-    <div class="input-group-btn">
-      <button class="btn btn-default" type="submit">
-        <i class="glyphicon glyphicon-search"></i>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselProducts" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true" style="filter: invert(100%);"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselProducts" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true" style="filter: invert(100%);"></span>
+        <span class="visually-hidden">Next</span>
       </button>
     </div>
-  </div>
-</form>
-  </div>
-</div>
 
-
-<header class="p-3 mb-3 border-bottom">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-        </a>
-
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 link-secondary">Overview</a></li>
-          <li><a href="#" class="nav-link px-2 link-dark">Inventory</a></li>
-          <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li>
-          <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>
-        </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-        </form>
-
-        <div class="dropdown text-end">
-          <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-          </a>
-          <ul class="dropdown-menu text-small">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Sign out</a></li>
-          </ul>
+    <div class="row">
+      <div class="col-3">
+        <h2>Buy again:</h2>
+        <div class="row">
+          <div class="col-auto">
+            <img src="image/gift.png" height="100" width="100">
+          </div>
+          <div class="col-auto">
+            <img src="image/blaster.png" height="100" width="100">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-auto">
+            <img src="image/house.png" height="100" width="100">
+          </div>
+          <div class="col-auto">
+            <img src="image/tshirt.png" height="100" width="100">
+          </div>
+        </div>
+      </div>
+    
+      <div class="col-3 text-center">
+        <h2>Evaluate your purchase:</h2>
+        <div class="">
+          <img src="image/house.png" height="100" width="100">
+          <img src="image/alien-star.png" height="40" width="40"> <!-- Da riposizionare -->
         </div>
       </div>
     </div>
-  </header>
+    
+    <div class="testimonial-group text-center">
+      <h2>Categories:</h2>
+      <div class="row">
+        <div class="col-auto">
+          <img src="image/gift.png" height="100" width="100">
+          <h5 class="fw-normal">Offert</h5>
+        </div>
+        <div class="col-auto">
+          <img src="image/blaster.png" height="100" width="100">
+          <h5 class="fw-normal">Technology</h5>
+        </div>
+        <div class="col-auto">
+          <img src="image/house.png" height="100" width="100">
+          <h5 class="fw-normal">House tools</h5>
+        </div>
+        <div class="col-auto">
+          <img src="image/tshirt.png" height="100" width="100">
+          <h5 class="fw-normal">Fashion</h5>
+        </div>
+        <div class="col-auto">
+          <img src="image/ufo.png" height="100" width="100">
+          <h5 class="fw-normal">Vehicle</h5>
+        </div>
+        <div class="col-auto">
+          <img src="image/food.png" height="100" width="100">
+          <h5 class="fw-normal">Food</h5>
+        </div>
+        <div class="col-auto">
+          <img src="image/art.png" height="100" width="100">
+          <h5 class="fw-normal">Art</h5>
+        </div>
+      </div>
+    </div>
+    <?php require "footer.php" ?>
+  </div>
 </body>
+
 </html>

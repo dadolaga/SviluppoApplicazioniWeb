db9b89a2186b83933adae5925dd990575573df85
Davide Lagazio <lagazio.davide.1f@gmail.com>
2023-07-13 21:11:22 +0200

    aggiunto under construction

---
+++ Progetto/home/underConstruction.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Under construction</title>
+    <?php include("../home/include.php") ?>
+    <style>
+        .center {
+            height: calc(100vh - 83px);
+        }
+    </style>
+</head>
+
+<body>
+    <header class="p-3 border-bottom " style="--bs-border-color: black;">
+        <div class="d-flex justify-content-between mx-3 align-items-center">
+            <div class="d-flex align-items-center">
+                <a href="../home/homepage.php" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                    <img src="../image/logo.jpg" height="50" width="50" alt="Logo">
+                </a>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0" style="color: black;">
+                    <li><a href="../home/homepage.php" class="nav-link px-2 "> Home </a></li>
+                    <li><a href="../product/product.php" class="nav-link px-2 "> Products </a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <div class="center d-flex justify-content-center align-items-center">
+        <div class="alert alert-info fade show">
+            <h4 class="alert-heading" ><i class="bi bi-info-circle-fill"></i> Oops! This page is under construction.</h4>
+            <div class="d-flex justify-content-center mt-3">
+                <h5>This page does not exist yet, please try again in a few days</h5>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
